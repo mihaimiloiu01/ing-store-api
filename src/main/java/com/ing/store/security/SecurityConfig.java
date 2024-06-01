e6aa4e44f8a219a828b4f7cbc1ae4e2601e1f37b
mmiloiu_deloitte <mmiloiu@deloittece.com>
2024-06-01 23:13:28 +0300

    feature: Fixed post and put endpoints

--- src/main/java/com/ing/store/security/SecurityConfig.java
+++ src/main/java/com/ing/store/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,7 +18,9 @@ public class SecurityConfig {
         .authorizeHttpRequests((authz) -> authz
             .anyRequest().permitAll()
         )
+        .csrf(AbstractHttpConfigurer::disable)
         .httpBasic(withDefaults());
+
     return http.build();
   }
 }
