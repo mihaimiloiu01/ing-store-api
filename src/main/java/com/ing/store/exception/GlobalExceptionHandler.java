53de0d94190d14742b085c3cdc582c675c1eb722
mmiloiu_deloitte <mmiloiu@deloittece.com>
2024-06-01 22:40:13 +0300

    feature: Added error handling

---
+++ src/main/java/com/ing/store/exception/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.ing.store.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException.BadRequest;
+import org.springframework.web.client.HttpClientErrorException.Unauthorized;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+  @ExceptionHandler(ProductNotFoundException.class)
+  public ResponseEntity<?> handleProductNotFoundException(ProductNotFoundException ex, WebRequest request) {
+    ErrorDetails errorDetails = new ErrorDetails(HttpStatus.NOT_FOUND, ex.getMessage(), request.getDescription(false));
+    return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(BadRequest.class)
+  public ResponseEntity<?> handleBadRequestException(BadRequest ex, WebRequest request) {
+    ErrorDetails errorDetails = new ErrorDetails(HttpStatus.BAD_REQUEST, ex.getMessage(), request.getDescription(false));
+    return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(Unauthorized.class)
+  public ResponseEntity<?> handleUnauthorizedException(Unauthorized ex, WebRequest request) {
+    ErrorDetails errorDetails = new ErrorDetails(HttpStatus.UNAUTHORIZED, ex.getMessage(), request.getDescription(false));
+    return new ResponseEntity<>(errorDetails, HttpStatus.UNAUTHORIZED);
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<?> handleGlobalException(Exception ex, WebRequest request) {
+    ErrorDetails errorDetails = new ErrorDetails(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), request.getDescription(false));
+    return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
