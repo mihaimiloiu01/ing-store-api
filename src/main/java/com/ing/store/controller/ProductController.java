32b5665a18355678fd63ca818afd87d5477ca2a5
mmiloiu_deloitte <mmiloiu@deloittece.com>
2024-06-01 22:56:47 +0300

    feature: Fixed unused imports

--- src/main/java/com/ing/store/controller/ProductController.java
+++ src/main/java/com/ing/store/controller/ProductController.java
@@ -6,7 +6,6 @@ import com.ing.store.utils.Constants;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
