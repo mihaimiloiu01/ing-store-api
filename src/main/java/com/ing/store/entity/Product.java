ab0e5a72b88d68f534159848f8ed99de679dec09
mmiloiu_deloitte <mmiloiu@deloittece.com>
2024-06-02 01:47:11 +0300

    feature: Added product deleting by id

--- src/main/java/com/ing/store/entity/Product.java
+++ src/main/java/com/ing/store/entity/Product.java
@@ -5,11 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.util.UUID;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "product")
 public class Product {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
